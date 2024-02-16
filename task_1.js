2dba8e4c55efe8b225354a8f41722febf3cd3d0f
wassupt2 <100017536+wassupt2@users.noreply.github.com>
2024-02-16 20:26:55 +0400

    Add files via upload

--- task_1.js
+++ task_1.js
@@ -1,9 +1,10 @@
-/*
-We introduced the standard function Math.min, which returns its smallest argument.
-We can build something like that now.
-Write a function min that takes two arguments and returns their minimum.
-
-ჩვენ ვისწავლეთ სტანდარტული ფუნქცია Math.min, რომელიც აბრუნებს უმცირეს არგუმენტს.
-ახლა შეგვიძლია მსგავსი ჩვენით ავაწყოთ.
-დაწერეთ ფუნქცია min რომელიც იღებს 2 არგუმენტს და აბრუნებს მათგან მინიმუმს.
-*/
+function min(a,b) {
+    if (a < b) {
+        console.log(a);
+    }if (a > b) {
+        console.log(b);
+    }
+}
+min(3, 2);
+min(7, 29);
+min(-18, -15);
